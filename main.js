9443c4a4f82e4f1360b3d2ff65b28295b6feb2fc
ihawp <ihawpebay@gmail.com>
2025-03-16 23:03:53 -0600

    presentation ready

--- main.js
+++ main.js
@@ -4,18 +4,21 @@ const options = {
     maximumAge: 0
 }
 
-// navigator.geolocation.getCurrentPosition(success, error, options);
-
-let l = navigator.geolocation.watchPosition(success, error, options);
-
-setTimeout(() => {
-    navigator.geolocation.clearWatch(l);
-    console.log('--- end ---');
-}, 10000);
-
 function success(pos) {
     console.log(pos);
 }
+
 function error(error) {
     console.error(error);
 }
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+/*
+let id = navigator.geolocation.watchPosition(success, error, options);
+
+setTimeout(() => {
+    navigator.geolocation.clearWatch(id);
+    console.log('end');
+}, 10000);
+*/
