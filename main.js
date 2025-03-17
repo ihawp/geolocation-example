c96db5452bcf33a1939c5f209f20806b8eea4d70
ihawp <ihawpebay@gmail.com>
2025-03-16 22:56:19 -0600

    Ready for presentation tomorrow!

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let l = navigator.geolocation.watchPosition(success, error, options);
 
 setTimeout(() => {
     navigator.geolocation.clearWatch(l);
-    console.log('we ended it!');
-}, 25000);
+    console.log('--- end ---');
+}, 10000);
 
 function success(pos) {
     console.log(pos);
